f4d531f0db1ddd693b36e51c6cecd088602762f0
RIDDIKULUS <alleyna1616@gmail.com>
2017-06-17 11:48:27 +0300

    Proje Github üzerine aktarıldı
    Projeler github üzerine aktarıldı.

---
+++ App001_Age.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main()
+{
+	char ad[10]="aleyna"; 
+	char soyad[10]="celebioglu";
+	int yas=20;
+	float boy=1.72;
+printf("adınız:%c\n  4.harfiniz :%c \n soyadınız: %c\n yaşınız:%d\n boyunuz: %.2f",ad,ad[3],soyad,yas,boy );
+  
+
+}
+
+	
+	
---
+++ Lesson003_Triangle.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main()
+{
+
+	printf("Hello World"); 
+	
+	for (int i = 0; i < 8; i++	)		
+	{
+		for (int j = 0; j < i; ++j)
+		{
+			printf("*");
+		}
+		
+		printf("\n");
+
+	}
+}
