80fd99dd4ed30be8a4c696b70c6a3677faa30efc
RIDDIKULUS <alleyna1616@gmail.com>
2017-06-20 19:29:03 +0300

    new
    find biggest number

---
+++ biggest_number.c
@@ -0,0 +1,18 @@
+/*#include<stdio.h>
+#include<stdlib.h>
+
+void main()
+{ // this app does show the biggest number .  
+
+	int a, b,c ;
+	printf("enter tree difrent number\n");
+	scanf("%d%d%d", &a, &b, &c);
+	if (a > b&&c) printf("the biggest number is = %d\n ", a);
+	else if (b>a&&b)  printf("the biggest number is = %d\n ", b);
+	else if (c>a&&b)  printf("the biggest number is = %d\n ", c );
+	 
+			
+
+	system("pause");
+
+}*/
