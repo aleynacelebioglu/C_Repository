1efc557fea9f1efbc95660353e036c3706fba1b4
RIDDIKULUS <alleyna1616@gmail.com>
2017-06-20 12:57:23 +0300

    add
    exercises added

---
+++ exercises.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void main()
+{
+	int a, b ;
+	float process = (a / b);
+	printf("Please enter two number...\n");
+		scanf ("%d\n%d", &a, &b);
+		if (a == 0 ||b == 0)
+			printf("Enter a non-zero number \n");
+		else
+		{
+			printf("The end of the process > %f ", process  );
+		}
+
+
+
+
+	system("pause");
+
+}
